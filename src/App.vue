ede3a5e371b3b42308432e661d639cbc90b8fd24
fgudino3 <frankyjg96@aol.com>
2019-03-06 10:55:17 -0800

    small vuetify fixes for the base page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <v-app id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <v-app>
+    <v-toolbar color="teal" dark app>
+      <v-toolbar-title>ACUA Ticketing Manager</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
@@ -13,25 +16,5 @@ import Vue from 'vue';
 export default Vue.extend({});
 </script>
 
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
